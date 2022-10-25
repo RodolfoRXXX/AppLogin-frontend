85ad751ab9bdd829c8fab3dba3ebe0284d5e59f4
Rodolfo <rodolfo.singular@gmail.com>
2022-10-25 10:48:38 -0300

    Estilizando los tags y páginas relacionadas

--- src/app/secure-componentes/tag-folder/alert-tag/alert-tag.component.ts
+++ src/app/secure-componentes/tag-folder/alert-tag/alert-tag.component.ts
@@ -78,7 +78,7 @@ export class AlertTagComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this._com.setTabEditor('Crear un alerta');
+    this._com.setTabEditor('Creando un alerta');
     this._activatedRoute.params.subscribe( (params: Params) => {
       this.tag.tipo = params['tipo'];
       this.tag.id   = params['id'];
