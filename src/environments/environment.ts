a962b5a258af59f756895e42b83b28ca6755afd2
Rodolfo <rodolfo.singular@gmail.com>
2023-02-27 08:56:16 -0300

    Cambio de settings en variables de entorno para backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  //URL : 'http://localhost:4000/',
-  //SERVER : 'http://localhost:4000/uploads/',
-  URL : 'https://www.vibrance.com.ar/',
-  SERVER : 'https://www.vibrance.com.ar/uploads/',
+  URL : 'http://localhost:4000/',
+  SERVER : 'http://localhost:4000/uploads/',
+  //URL : 'https://www.vibrance.com.ar/',
+  //SERVER : 'https://www.vibrance.com.ar/uploads/',
 };
 
 /*
