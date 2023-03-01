326fad34f91f80c2ce184b449b37915d5d660745
Rodolfo <rodolfo.singular@gmail.com>
2023-03-01 11:45:28 -0300

    Angular material instalado y navbar de prueba creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { NavbarComponent } from './shared/components/header/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     MaterialModule,
     BrowserAnimationsModule
   ],
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ],
   exports: [ //aquí se exportan los módulos de Material que queremos usar
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule { }
